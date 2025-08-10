e489de7b865a9aeccb6846d032e79d346eea3f73
Sarthak210905 <sarthakchoukse2109@gmail.com>
2025-08-10 14:54:34 +0000

    logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,11 +6,8 @@ const Logo = ({ className }: { className?: string }) => {
   return (
     <Link href="/" className={cn("flex items-center gap-2", className)}>
       <Image
-<<<<<<< HEAD
+
         src="/logo.jpg" // Path to your logo in public folder
-=======
-        src="/logo.png" // Path to your logo in public folder
->>>>>>> 12dad75c06410c81dad5458b3a6a88f30e5c824a
         alt="Prency Hangers Logo"
         width={28} // Adjust size
         height={28}
